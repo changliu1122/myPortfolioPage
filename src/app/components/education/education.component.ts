408c3223b36cb69e158816b0f8462ec5a1a84443
CHANG LIU <120937649+changliu1122@users.noreply.github.com>
2024-11-18 00:10:19 +0100

    add picture and finished layout

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit  } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {MatStepper, MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [
+    MatButtonModule,  
+    MatStepperModule,
+    MatIconModule,
+  
+  ],
   templateUrl: './education.component.html',
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
+  
+  @ViewChild('stepper') stepper!: MatStepper;
 
+  ngAfterViewInit() {
+    // Wait for view to be fully initialized
+    setTimeout(() => {
+      // Open all steps
+      for(let i = 0; i < this.stepper._steps.length; i++) {
+        this.stepper.selected = this.stepper._steps.get(i);
+      }
+    });
+  }
 }
--- src/app/components/working-experience/working-experience.component.ts
+++ src/app/components/working-experience/working-experience.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatStepperModule } from '@angular/material/stepper';
 @Component({
   selector: 'app-working-experience',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatStepperModule],
   templateUrl: './working-experience.component.html',
   styleUrl: './working-experience.component.css'
 })
